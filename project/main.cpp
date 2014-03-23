57b634c5ce580c912c82dfa373d7a32ea220ce0f
gregpaton08 <gregpaton08@gmail.com>
2014-03-22 21:48:15 -0400

    Initial Commit

---
+++ project/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  project
+//
+//  Created by Greg Paton on 3/22/14.
+//  Copyright (c) 2014 Greg Paton. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
